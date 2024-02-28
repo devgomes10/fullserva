f13499d917eceb1e17b95a67e531578fc99747e3
Vinicius Gomes Correia <viniciusgomesccc10@gmail.com>
2024-02-27 23:37:21 -0300

    implementação de gerenciamento de colaboradores

--- lib/views/employees/employee_form_view.dart
+++ lib/views/employees/employee_form_view.dart
@@ -15,7 +15,7 @@ class EmployeeFormView extends StatefulWidget {
 }
 
 class _EmployeeFormViewState extends State<EmployeeFormView> {
-  String uniqueId = const Uuid().v4();
+  String _uniqueId = const Uuid().v4();
   final _formKey = GlobalKey<FormState>();
   EmployeeController _employeeController = EmployeeController();
   ServiceController _serviceController = ServiceController();
@@ -25,6 +25,24 @@ class _EmployeeFormViewState extends State<EmployeeFormView> {
   final _phoneController = TextEditingController();
   List<String> _selectedServicesIds = [];
   List<String> _servicesIdList = [];
+  int _selectedRole = -1;
+  List<bool> _selectionsRole = [false, false, false];
+  ValueNotifier<int> _selectedOptionIndex = ValueNotifier<int>(-1);
+
+  void _updateSelectedRole(int i) {
+    setState(() {
+      for (int buttonIndex = 0;
+          buttonIndex < _selectionsRole.length;
+          buttonIndex++) {
+        if (buttonIndex == i) {
+          _selectionsRole[buttonIndex] = true; // Seleciona o botão pressionado
+        } else {
+          _selectionsRole[buttonIndex] = false; // Desseleciona os outros botões
+        }
+      }
+      _selectedRole = i; // Atualiza o índice da opção selecionada
+    });
+  }
 
   @override
   void initState() {
@@ -34,6 +52,7 @@ class _EmployeeFormViewState extends State<EmployeeFormView> {
       _emailController.text = widget.model!.email;
       _passwordController.text = widget.model!.password;
       _phoneController.text = widget.model!.phone;
+      _selectedOptionIndex.value = widget.model!.role;
     }
     _fetchAvailableServices();
   }
@@ -48,9 +67,21 @@ class _EmployeeFormViewState extends State<EmployeeFormView> {
 
   @override
   Widget build(BuildContext context) {
+    final employeeModel = widget.model;
     return Scaffold(
       appBar: AppBar(
         title: Text("Equipe"),
+        actions: employeeModel != null
+            ? [
+                IconButton(
+                  onPressed: () async {
+                    await _employeeController.removeEmployee(employeeModel.id);
+                    Navigator.pop(context);
+                  },
+                  icon: Icon(Icons.delete),
+                ),
+              ]
+            : null,
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -58,6 +89,31 @@ class _EmployeeFormViewState extends State<EmployeeFormView> {
           key: _formKey,
           child: Column(
             children: [
+              ValueListenableBuilder<int>(
+                valueListenable: _selectedOptionIndex,
+                builder: (context, selectedIndex, _) {
+                  return ToggleButtons(
+                    children: <Widget>[
+                      Text('Operacional'),
+                      Text('Assistencial'),
+                      Text('Administrativo'),
+                    ],
+                    isSelected: [
+                      selectedIndex == 0,
+                      selectedIndex == 1,
+                      selectedIndex == 2
+                    ],
+                    onPressed: (int index) {
+                      _selectedOptionIndex.value =
+                          index; // Atualiza o valor do índice selecionado
+                      // Após atualizar o índice selecionado, você pode realizar outras operações aqui
+                      // Se necessário, como salvar o valor selecionado em uma variável int
+                      int selectedValue = index;
+                      print('Valor selecionado: $selectedValue');
+                    },
+                  );
+                },
+              ),
               TextFormField(
                 keyboardType: TextInputType.text,
                 controller: _nameController,
@@ -73,7 +129,7 @@ class _EmployeeFormViewState extends State<EmployeeFormView> {
                 keyboardType: TextInputType.emailAddress,
                 controller: _emailController,
                 decoration:
-                const InputDecoration(labelText: 'E-mail de acesso'),
+                    const InputDecoration(labelText: 'E-mail de acesso'),
                 validator: (value) {
                   if (value == null || value.isEmpty) {
                     return 'Obrigatório';
@@ -84,8 +140,7 @@ class _EmployeeFormViewState extends State<EmployeeFormView> {
               TextFormField(
                 keyboardType: TextInputType.text,
                 controller: _passwordController,
-                decoration:
-                const InputDecoration(labelText: 'Senha de acesso'),
+                decoration: const InputDecoration(labelText: 'Senha de acesso'),
                 validator: (value) {
                   if (value == null || value.isEmpty) {
                     return 'Obrigatório';
@@ -97,7 +152,7 @@ class _EmployeeFormViewState extends State<EmployeeFormView> {
                 keyboardType: TextInputType.phone,
                 controller: _phoneController,
                 decoration:
-                const InputDecoration(labelText: 'Telefone do Colaborador'),
+                    const InputDecoration(labelText: 'Telefone do Colaborador'),
                 validator: (value) {
                   if (value == null || value.isEmpty) {
                     return 'Obrigatório';
@@ -106,57 +161,82 @@ class _EmployeeFormViewState extends State<EmployeeFormView> {
                 },
               ),
               Text("Atribuir serviços"),
-              Expanded(
-                child: Container(
-                  width: MediaQuery.of(context).size.width,
-                  decoration: BoxDecoration(border: Border.all(width: 5)),
-                  child: StreamBuilder<List<Service>>(
-                    stream: _serviceController.getService(),
-                    builder: (context, snapshot) {
-                      if (snapshot.connectionState == ConnectionState.waiting) {
-                        return const Center(child: CircularProgressIndicator());
-                      }
-                      if (snapshot.hasError) {
-                        return const Center(
-                          // Adicionar uma imagem
-                          child: Text('Erro ao carregar os dados'),
-                        );
-                      }
-                      final services = snapshot.data;
-                      if (services == null || services.isEmpty) {
+              Container(
+                width: MediaQuery.of(context).size.width,
+                height: 200,
+                decoration: BoxDecoration(border: Border.all(width: 5)),
+                child: StreamBuilder<List<Service>>(
+                  stream: _serviceController.getService(),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const Center(child: CircularProgressIndicator());
+                    }
+                    if (snapshot.hasError) {
+                      return const Center(
                         // Adicionar uma imagem
-                        return const Center(
-                          child: Text('Nenhum dado disponível'),
-                        );
-                      }
-                      return ListView.builder(
-                        itemCount: services.length,
-                        itemBuilder: (context, index) {
-                          final service = services[index];
-                          final isSelected = _selectedServicesIds.contains(service.id);
-                          final isSelectedNotifier = ValueNotifier<bool>(isSelected);
-                          return ValueListenableBuilder<bool>(
-                            valueListenable: isSelectedNotifier,
-                            builder: (context, value, child) {
-                              return CheckboxListTile(
-                                title: Text(service.name),
-                                value: value,
-                                onChanged: (bool? newValue) {
-                                  isSelectedNotifier.value = newValue!;
-                                  if (newValue) {
-                                    _selectedServicesIds.add(service.id);
-                                  } else {
-                                    _selectedServicesIds.remove(service.id);
-                                  }
-                                },
-                              );
-                            },
-                          );
-                        },
+                        child: Text('Erro ao carregar os dados'),
                       );
-                    },
-                  ),
+                    }
+                    final services = snapshot.data;
+                    if (services == null || services.isEmpty) {
+                      // Adicionar uma imagem
+                      return const Center(
+                        child: Text('Nenhum dado disponível'),
+                      );
+                    }
+                    return ListView.builder(
+                      itemCount: services.length,
+                      itemBuilder: (context, index) {
+                        final service = services[index];
+                        final isSelected =
+                            _selectedServicesIds.contains(service.id);
+                        final isSelectedNotifier =
+                            ValueNotifier<bool>(isSelected);
+                        return ValueListenableBuilder<bool>(
+                          valueListenable: isSelectedNotifier,
+                          builder: (context, value, child) {
+                            return CheckboxListTile(
+                              title: Text(service.name),
+                              value: value,
+                              onChanged: (bool? newValue) {
+                                isSelectedNotifier.value = newValue!;
+                                if (newValue) {
+                                  _selectedServicesIds.add(service.id);
+                                } else {
+                                  _selectedServicesIds.remove(service.id);
+                                }
+                              },
+                            );
+                          },
+                        );
+                      },
+                    );
+                  },
                 ),
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  int selectedValue = _selectedOptionIndex.value;
+                  if (_formKey.currentState!.validate()) {
+                    Employee employee = Employee(
+                      id: _uniqueId,
+                      name: _nameController.text,
+                      email: _emailController.text,
+                      password: _passwordController.text,
+                      phone: _phoneController.text,
+                      role: selectedValue,
+                      serviceIds: _selectedServicesIds,
+                    );
+
+                    if (employeeModel != null) {
+                      await _employeeController.updateEmployee(employee);
+                    } else {
+                      await _employeeController.addEmployee(employee);
+                    }
+                    Navigator.pop(context, true);
+                  }
+                },
+                child: Text("CONFIRMAR"),
               ),
             ],
           ),
